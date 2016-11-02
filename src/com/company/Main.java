eba15865225f6564544bffdf7f34c031f1dfb1cf
df377 <df377@pvsd.org>
2016-11-02 14:20:14 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+//Lesson20
+public class Main {
+
+    public static void main(String[] args) {
+
+        //static methods sometimes called class methods
+        //Class- more broad
+            //Object- more specific
+
+        //instance fields aka static variables
+
+        //New way to call method
+            //Add static to method or instance field
+            //NO OBJECT NEEDED
+                //Ex- Nerd.methodB("something");
+
+        //Static methods used all the time
+            //Math.random()
+            //Arrays.sort
+
+    }
+}
